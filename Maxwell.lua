3f530890d249f362694643dd01ec633885aa5229
FloofyWoo <52764294+FloofyWoo@users.noreply.github.com>
2023-02-17 23:24:39 -0500

    Update Maxwell.lua

--- Maxwell.lua
+++ Maxwell.lua
@@ -1,17 +1,15 @@
-game:GetObjects(getsynasset("Models/Maxwell.rbxm"))[1].Parent = game.Players.LocalPlayer.Backpack
-
 local GetAsset = getcustomasset or getsynasset
 
 local lp = game.Players.LocalPlayer.Name
-
 local e = game.Workspace[""..lp..""]:WaitForChild(cat); 
+local on = false
 
 e.Activated:Connect(function() --Tool activation function
-    e.Handle.Sound.SoundId = GetAsset("Models/Maxwell.mp3") 
-    e.Handle["Sound"].Playing = not e.Handle["Sound"].Playing
-    if on == false then
-        on = true
-    elseif on == true then
-        on = false
-    end
+	e.Handle.Sound.SoundId = GetAsset("Models/Maxwell.mp3") 
+	e.Handle["Sound"].Playing = not e.Handle["Sound"].Playing
+	if on == false then
+		on = true
+	elseif on == true then
+		on = false
+	end
 end)
