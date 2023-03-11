729a07aa3d3255f2c07fedbdcee80bd621f9113a
FloofyWoo <52764294+FloofyWoo@users.noreply.github.com>
2023-03-11 11:38:03 -0500

    Create SERVERYEETER.lua

---
+++ SERVERYEETER.lua
@@ -0,0 +1,56 @@
+Players = game:WaitForChild("Players")
+local Character
+local Player
+local Humanoid
+local HumanoidRootPart
+
+local lplr = game.Players.LocalPlayer.Name
+Tool = game.Workspace[""..lplr..""]:WaitForChild("ServerYeeter")
+Handle = Tool:WaitForChild("Handle")
+
+ToolEquipped = false
+
+
+function Activated()
+	if ToolEquipped == true then
+		if Humanoid.Health ~= 0 then
+			for i,v in pairs(game:GetService("Workspace"):GetDescendants())do
+				if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation") or v:IsA("WedgePart") then
+					v.Color = Color3.fromRGB(0,0,0)
+					v.Material = Enum.Material.CorrodedMetal
+					a = Instance.new('Explosion')
+					a.BlastRadius = 10
+					a.BlastPressure = 50000
+
+				end
+			end
+			while wait(0) do
+				for i,v in pairs(game:GetService("Workspace"):GetDescendants())do
+					if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation") or v:IsA("WedgePart") then
+						wait(0.1)
+						v.Anchored = false
+						a:Clone().Parent = v
+					end
+				end
+			end
+		end
+	end
+end
+
+
+
+function Equipped()
+	Character = Tool.Parent
+	Player = Players:GetPlayerFromCharacter(Character)
+	Humanoid = Character:FindFirstChildOfClass("Humanoid")
+	HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
+	ToolEquipped = true
+end
+
+function Unequipped()
+	ToolEquipped = false
+end
+
+Tool.Activated:Connect(Activated)
+Tool.Equipped:Connect(Equipped)
+Tool.Unequipped:Connect(Unequipped)
